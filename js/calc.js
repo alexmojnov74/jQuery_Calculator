e816e771b95a35aa8d901df595dc67d2d8fd4878
Alex Mojnov <alexmojnov@gmail.com>
2019-06-14 15:59:23 -0400

    added styles for the calculator

---
+++ js/calc.js
@@ -0,0 +1,29 @@
+$(document).ready(function(e) {
+
+  let num1;
+  let num2;
+  let ops = "";
+  let answer;
+  let opselected = false;
+  let calculated = false;
+  
+      
+      $('button').on('click', function(event) {
+        $('#answer').text($(this).text());
+        
+        
+      
+    
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    })
